11cfee071cd155c9c9ca5aa021c1e8d3b38e6523
ICanGamez <Trentity2000@gmail.com>
2026-05-04 23:15:53 -0500

    bugfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router'; // Not @docusaurus/core
 
 export default function Home() {
-  return <Redirect to="/intro"/>;
+  return <Redirect to="datapacker-s-guide-to-minecraft/intro"/>;
 }
