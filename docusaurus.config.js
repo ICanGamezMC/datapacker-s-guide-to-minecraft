21ba09628706bd47ad9279df4f598c23f77910e3
ICanGamez <Trentity2000@gmail.com>
2026-05-08 17:18:42 -0500

    bug fix on logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'DGTM',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/logo.png',
+  favicon: '/img/logo.png',
   
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -72,7 +72,7 @@ const config = {
         title: 'DGTM',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: '/img/logo.svg',
         },
         items: [
           {
