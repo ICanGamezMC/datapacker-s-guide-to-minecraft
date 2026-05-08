bd7af4ac6fb12d55e9e44304dd0d42471d20e871
ICanGamez <Trentity2000@gmail.com>
2026-05-08 17:22:03 -0500

    fix on logo header.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'DGTM',
   tagline: 'Dinosaurs are cool',
-  favicon: '/img/logo.png',
+  favicon: 'img/logo.ico',
   
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
