d19566aec7eca6bbe077f1e50bbaea8634b87670
ICanGamez <Trentity2000@gmail.com>
2026-05-08 17:05:22 -0500

    new logo for website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'DGTM',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
   
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
